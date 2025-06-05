6c5a397808765057b8a1a07ba223171c9685fcd7
Shemdoe <georgehmariki@gmail.com>
2025-06-06 01:18:47 +0300

    testing knowledge base

--- functions/gemini.js
+++ functions/gemini.js
@@ -1,19 +1,30 @@
 const { GoogleGenAI, createUserContent, createPartFromUri } = require('@google/genai');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const GEMINI_API_KEY = process.env.GEMINI_KEY;
 console.log(`This is your Gemini Key: ${GEMINI_API_KEY}`)
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
-const GeminiResponse = async (prompt) => {
-    console.log(`This is your Gemini Key: ${GEMINI_API_KEY}`)
+// Function to load knowledge base
+
+const GeminiResponse = async (user_prompt) => {
     try {
+        //knowledge base
+        const kbPath = path.join(__dirname, '../database/kb.txt') || null;
+        const kb = await ai.files.upload({
+            file: kbPath, config: {mimeType: 'text/plain' }
+        })
         const response = await ai.models.generateContent({
             model: 'gemini-2.0-flash-lite',
-            contents: prompt,
+            contents: createUserContent([
+                createPartFromUri(kb.uri, kb.mimeType),
+                user_prompt
+            ]),
         });
-        console.log(response.text);
+        
         return {
             status: 'success',
             message: response.text,
