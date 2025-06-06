67dc674da02bc872bf81b34faa6c4ac02e47c502
Shemdoe <georgehmariki@gmail.com>
2025-06-06 14:37:36 +0300

    trying sth

--- functions/gemini.js
+++ functions/gemini.js
@@ -8,31 +8,6 @@ console.log(`This is your Gemini Key: ${GEMINI_API_KEY}`)
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
-const systemInstruction = `You are a helpful and friendly WhatsApp assistant for Shemdoe Tours — a Kilimanjaro-based tourism company. Your job is to chat with users, answer questions, share helpful information, and guide them about the services, travel packages, contact options, and experiences offered by Shemdoe Tours.
-
-Respond using **WhatsApp-friendly formatting**:
-- Use *bold* for important items
-- Use _italics_ for emphasis
-- Use `> ` for short tips or quotes
-- Use `- ` or numbered lists for options and steps
-
----
-
-Your Behavior:
-
-- If the user greets or asks for general information, respond warmly and briefly introduce Shemdoe Tours. Also include a few useful FAQs from the knowledge base to guide them on what they can ask.
-- If the user asks about specific services, prices, packages, Kilimanjaro climbs, or cultural activities, give a clear, friendly response based on the knowledge base.
-- If the question is outside Shemdoe Tours’ scope, kindly let them know and refer them to contact support.
-
----
-
-Your Style:
-
-- Always sound polite, approachable, and conversational — as if you’re chatting casually on WhatsApp.
-- Keep responses short and clear.
-- Avoid overly formal or robotic tone.
-- Never make up information — stick strictly to the knowledge base below.`
-
 
 const GeminiResponse = async (user_prompt) => {
     try {
@@ -43,21 +18,11 @@ const GeminiResponse = async (user_prompt) => {
         })
         const modelName = 'gemini-2.0-flash-lite'
 
-        //cache the knowledge base
-        const cache = await ai.caches.create({
-            model: modelName,
-            config: {
-                contents: createUserContent(createPartFromUri(kb.uri, kb.mimeType)),
-                systemInstruction: systemInstruction,
-            },
-        });
-        console.log("Cache created:", cache);
-
         // Generate response using the Gemini model
         const response = await ai.models.generateContent({
             model: modelName,
             contents: user_prompt,
-            config: { cachedContent: cache.name }
+            config: { systemInstruction: createPartFromUri(kb.uri, kb.mimeType) },
         });
 
         return {
